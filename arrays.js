6d9a41394f1e65fd3f7e14fc25a6fb9e1a5357d6
Noah Berman <noahberm@gmail.com>
2018-05-13 19:32:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-var chocolateBars = new Array ("snickers", "hundred grand", "kitkat", "skittles")
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningofArray (array, element) {
   return newArray = [${element}, ...${array}]
