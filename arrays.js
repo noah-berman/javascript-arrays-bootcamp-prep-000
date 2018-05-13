b51e58844fd66f77fb1585f4a76e5b9336e0b429
Noah Berman <noahberm@gmail.com>
2018-05-13 19:25:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,3 +7,11 @@ function addElementToBeginningofArray (array, element) {
 function destructivelyAddElementToBeginningOfArray (array, element) {
   return ${array} = [${element}, ...${array}]
 }
+
+function addElementToEndofArray (array, element) {
+  return newArray = [...${array}, ${element} ]
+}
+
+function destructivelyAddElementToEndOfArray (array, element) {
+  return ${array} = [${element}, ...${array}]
+}
